3738bacc984770d7de67a85745ccd792eb3500bd
paulleerentveld <paul@veldnet.com.au>
2022-05-12 09:32:27 +1000

    set image container width to image width and hide overflow

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ function initComparisons() {
     /*get the width and height of the img element*/
     w = img.offsetWidth;
     h = img.offsetHeight;
+    // set image container width
+    const imageContainer = document.getElementById("img-container");
+    imageContainer.style.width = w + "px";
+    imageContainer.style.height = h + "px";
+    console.log(imageContainer.style.width + imageContainer.style.height);
     /*set the width of the img element to 50%:*/
     img.style.width = (w / 2) + "px";
     /*create slider:*/
